7e45730e8cf9ced691214e01bee08bd1dda069d3
Farukcan Uğurlu <101269959+farukcanugurlu@users.noreply.github.com>
2023-05-19 21:32:38 +0300

    Student Registration Form with Database Integration

--- odev5/studentregister.php
+++ odev5/studentregister.php
@@ -14,7 +14,6 @@ $password = "";
 
 try {
     $db = new PDO("mysql:host=$servername;dbname=students", $username, $password);
-    // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
